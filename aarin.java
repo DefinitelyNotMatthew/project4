4af9921de5185b585f187dbca872c55b6e4d9bab
Matthew <matthewnguyen156@gmail.com>
2025-05-15 01:34:06 -0500

    aarin

---
+++ aarin.java
@@ -0,0 +1,12 @@
+public class aarin
+{
+    public static void main(String[] args)
+    {
+        aarin jibbly = new aarin();
+        aarin.doAbsolutelyNothing();
+    }
+    public void doAbsolutelyNothing()
+    {
+        String ough = "oughhhh";
+    }
+}
